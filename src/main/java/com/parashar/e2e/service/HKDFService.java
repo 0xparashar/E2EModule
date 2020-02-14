b27ec9a2a00659503057e19209ed0d3f7912b8a4
Ankit Parashar <ankitabc12@gmail.com>
2020-02-14 16:29:56 +0530

    cleaning up

--- src/main/java/com/parashar/e2e/service/HKDFService.java
+++ src/main/java/com/parashar/e2e/service/HKDFService.java
@@ -87,45 +87,4 @@ public class HKDFService {
         return bb.array();
     }
     
-    public String encryptData(EncryptionSpec encryptionSpec) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException{
-        SecretKey secretKey = new SecretKeySpec(Base64.getDecoder().decode(encryptionSpec.getSessionKey()), "AES");
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] iv = new byte[12]; //NEVER REUSE THIS IV WITH SAME KEY
-        secureRandom.nextBytes(iv);
-        final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-        GCMParameterSpec parameterSpec = new GCMParameterSpec(128, iv); //128 bit auth tag length
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
-        byte[] cipherText = cipher.doFinal(encryptionSpec.getXml().getBytes(StandardCharsets.UTF_8));
-        ByteBuffer byteBuffer = ByteBuffer.allocate(4 + iv.length + cipherText.length);
-        byteBuffer.putInt(iv.length);
-        byteBuffer.put(iv);
-        byteBuffer.put(cipherText);
-        byte[] cipherMessage = byteBuffer.array();
-        return Base64.getEncoder().encodeToString(cipherMessage);
-    }
-
-    public String decryptData(DecryptionSpec decryptionSpec) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException{
-        
-        
-        ByteBuffer byteBuffer = ByteBuffer.wrap(Base64.getDecoder().decode(decryptionSpec.getEncryptedData()));
-        
-        SecretKey secretKey = new SecretKeySpec(Base64.getDecoder().decode(decryptionSpec.getSessionKey()), "AES");
-        
-        int ivLength = byteBuffer.getInt();
-        if(ivLength < 12 || ivLength >= 16) { // check input parameter
-            throw new IllegalArgumentException("invalid iv length");
-        }
-        byte[] iv = new byte[ivLength];
-        byteBuffer.get(iv);
-        byte[] cipherText = new byte[byteBuffer.remaining()];
-        byteBuffer.get(cipherText);
-        
-        final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, new GCMParameterSpec(128, iv));
-        
-        byte[] plainText= cipher.doFinal(cipherText);
-
-        return new String(plainText);
-    }
-    
 }
